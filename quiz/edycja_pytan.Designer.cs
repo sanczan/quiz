a45328518267138bd7cdde689e126649f9c33839
sanczan <slawekszulik12345@gmail.com>
2023-05-22 18:57:56 +0200

    Add button that inserts data

--- quiz/edycja_pytan.Designer.cs
+++ quiz/edycja_pytan.Designer.cs
@@ -36,9 +36,11 @@
             this.odpowiedz_czwarta = new System.Windows.Forms.TextBox();
             this.odpowiedz_trzecia = new System.Windows.Forms.TextBox();
             this.odpowiedz_druga = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_dodaj = new System.Windows.Forms.Button();
             this.odpowiedz_pierwsza = new System.Windows.Forms.TextBox();
             this.comboBox_edycja_pytania = new System.Windows.Forms.ComboBox();
+            this.button_modyfikuj = new System.Windows.Forms.Button();
+            this.button_usun = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label5
@@ -88,42 +90,41 @@
             // 
             // odpowiedz_czwarta
             // 
-            this.odpowiedz_czwarta.Enabled = false;
             this.odpowiedz_czwarta.Location = new System.Drawing.Point(229, 257);
             this.odpowiedz_czwarta.Name = "odpowiedz_czwarta";
-            this.odpowiedz_czwarta.Size = new System.Drawing.Size(100, 20);
-            this.odpowiedz_czwarta.TabIndex = 17;
+            this.odpowiedz_czwarta.Size = new System.Drawing.Size(121, 20);
+            this.odpowiedz_czwarta.TabIndex = 5;
             // 
             // odpowiedz_trzecia
             // 
-            this.odpowiedz_trzecia.Enabled = false;
             this.odpowiedz_trzecia.Location = new System.Drawing.Point(229, 211);
             this.odpowiedz_trzecia.Name = "odpowiedz_trzecia";
-            this.odpowiedz_trzecia.Size = new System.Drawing.Size(100, 20);
-            this.odpowiedz_trzecia.TabIndex = 16;
+            this.odpowiedz_trzecia.Size = new System.Drawing.Size(121, 20);
+            this.odpowiedz_trzecia.TabIndex = 4;
             // 
             // odpowiedz_druga
             // 
             this.odpowiedz_druga.Location = new System.Drawing.Point(229, 168);
             this.odpowiedz_druga.Name = "odpowiedz_druga";
-            this.odpowiedz_druga.Size = new System.Drawing.Size(100, 20);
-            this.odpowiedz_druga.TabIndex = 15;
+            this.odpowiedz_druga.Size = new System.Drawing.Size(121, 20);
+            this.odpowiedz_druga.TabIndex = 3;
             // 
-            // button1
+            // button_dodaj
             // 
-            this.button1.Location = new System.Drawing.Point(229, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Dodaj";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_dodaj.Location = new System.Drawing.Point(120, 332);
+            this.button_dodaj.Name = "button_dodaj";
+            this.button_dodaj.Size = new System.Drawing.Size(75, 23);
+            this.button_dodaj.TabIndex = 6;
+            this.button_dodaj.Text = "Dodaj";
+            this.button_dodaj.UseVisualStyleBackColor = true;
+            this.button_dodaj.Click += new System.EventHandler(this.button_dodaj_Click);
             // 
             // odpowiedz_pierwsza
             // 
             this.odpowiedz_pierwsza.Location = new System.Drawing.Point(229, 120);
             this.odpowiedz_pierwsza.Name = "odpowiedz_pierwsza";
-            this.odpowiedz_pierwsza.Size = new System.Drawing.Size(100, 20);
-            this.odpowiedz_pierwsza.TabIndex = 13;
+            this.odpowiedz_pierwsza.Size = new System.Drawing.Size(121, 20);
+            this.odpowiedz_pierwsza.TabIndex = 2;
             // 
             // comboBox_edycja_pytania
             // 
@@ -131,14 +132,34 @@
             this.comboBox_edycja_pytania.Location = new System.Drawing.Point(229, 72);
             this.comboBox_edycja_pytania.Name = "comboBox_edycja_pytania";
             this.comboBox_edycja_pytania.Size = new System.Drawing.Size(121, 21);
-            this.comboBox_edycja_pytania.TabIndex = 23;
+            this.comboBox_edycja_pytania.TabIndex = 1;
             this.comboBox_edycja_pytania.SelectionChangeCommitted += new System.EventHandler(this.comboBox_edycja_pytania_SelectionChangeCommitted);
+            // 
+            // button_modyfikuj
+            // 
+            this.button_modyfikuj.Location = new System.Drawing.Point(229, 332);
+            this.button_modyfikuj.Name = "button_modyfikuj";
+            this.button_modyfikuj.Size = new System.Drawing.Size(75, 23);
+            this.button_modyfikuj.TabIndex = 7;
+            this.button_modyfikuj.Text = "Modyfikuj";
+            this.button_modyfikuj.UseVisualStyleBackColor = true;
+            // 
+            // button_usun
+            // 
+            this.button_usun.Location = new System.Drawing.Point(340, 332);
+            this.button_usun.Name = "button_usun";
+            this.button_usun.Size = new System.Drawing.Size(75, 23);
+            this.button_usun.TabIndex = 8;
+            this.button_usun.Text = "Usun";
+            this.button_usun.UseVisualStyleBackColor = true;
             // 
             // edycja_pytan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_usun);
+            this.Controls.Add(this.button_modyfikuj);
             this.Controls.Add(this.comboBox_edycja_pytania);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -148,7 +169,7 @@
             this.Controls.Add(this.odpowiedz_czwarta);
             this.Controls.Add(this.odpowiedz_trzecia);
             this.Controls.Add(this.odpowiedz_druga);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_dodaj);
             this.Controls.Add(this.odpowiedz_pierwsza);
             this.Name = "edycja_pytan";
             this.Text = "edycja_pytan";
@@ -167,8 +188,10 @@
         private System.Windows.Forms.TextBox odpowiedz_czwarta;
         private System.Windows.Forms.TextBox odpowiedz_trzecia;
         private System.Windows.Forms.TextBox odpowiedz_druga;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_dodaj;
         private System.Windows.Forms.TextBox odpowiedz_pierwsza;
         private System.Windows.Forms.ComboBox comboBox_edycja_pytania;
+        private System.Windows.Forms.Button button_modyfikuj;
+        private System.Windows.Forms.Button button_usun;
     }
 }
--- quiz/edycja_pytan.cs
+++ quiz/edycja_pytan.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
+using System.Security.Cryptography.X509Certificates;
 
 namespace quiz
 {
@@ -21,77 +21,72 @@ namespace quiz
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //string connectionString = @"Data Source=D:\visual\projekty\quiz\quiz\quizy_database.db;Version=3;"; //dostanie sie do pliku .db
-
-            string connectionString = @"Data Source=D:\visual\projekty\quiz\quiz\quizy_database.db;Version=3;";
-            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-            {
-                connection.Open();
-
-                string sql = "INSERT INTO TableName (Column1, Column2) VALUES (@Value1, @Value2)";
-
-                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
-                {
-                    // Set the parameter values
-                    command.Parameters.AddWithValue("@Value1", odpowiedz_pierwsza);
-                    command.Parameters.AddWithValue("@Value2", odpowiedz_druga);
-
-                    // Execute the command
-                    command.ExecuteNonQuery();
-                }
-
-                //connection.Insert(temp);    //zapis do pliku
-
-                connection.Close();
-                // Perform the data insertion
-            }
-
-            
-        }
+        public int id_quizu_na_podstawie_nazwy_quizu;   //distanie sie na upo do id quizu
 
         private void edycja_pytan_Load(object sender, EventArgs e)
         {
             string connectionString = @"Data Source=D:\visual\projekty\quiz\quiz\quizy_database.db;Version=3;"; //dostanie sie do pliku .db
+
+
+            //---------------id quizu---------------
+
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))    //utworzenie polaczenia
             {
                 connection.Open();
 
-                string nazwa = wybor_quizu.nazwa_quizu;
+                string sql = "SELECT id_quiz FROM quizy WHERE nazwa='" + wybor_quizu.nazwa_quizu + "'"; //zapytanie
 
-                string query = "SELECT pytania.tresc from pytania INNER JOIN quizy ON pytania.id_quiz = quizy.id_quiz WHERE quizy.nazwa='"+ nazwa + "'";   //sformuowanie zapytania
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
 
-                //select pytania.tresc from pytania INNER JOIN quizy ON pytania.id_quiz = quizy.id_quiz WHERE quizy.nazwa ="Quiz 1";
-
-                SQLiteCommand command = new SQLiteCommand(query, connection);
-                
                 SQLiteDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
                 {
+                    id_quizu_na_podstawie_nazwy_quizu = reader.GetInt32(0);
+                }
+
+                connection.Close();
+            }
+
+
+            //-------------------wypunktowanie listy w comboboxie---------------------
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))    //utworzenie polaczenia
+            {
+                connection.Open();
+
+                string nazwa = wybor_quizu.nazwa_quizu; //nazwa quizu z poprzedniego forma
+
+                string query = "SELECT pytania.tresc from pytania INNER JOIN quizy ON pytania.id_quiz = quizy.id_quiz WHERE quizy.nazwa='" + nazwa + "'";   //sformuowanie zapytania
+
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())   //dodanie elementow do comboboxa
+                {
                     string name = reader.GetString(0);
                     comboBox_edycja_pytania.Items.Add(name);
                 }
-                
+
                 connection.Close();
             }
 
         }
 
+        public int id;  //id danego pytania (na przyszlosc do edycji)
+
+        //---------------------------------klikniecie elementu w vomboboxie pokazuje elementy w textboxach--------------------------------------
+
         private void comboBox_edycja_pytania_SelectionChangeCommitted(object sender, EventArgs e)
         {
+
             string connectionString = @"Data Source=D:\visual\projekty\quiz\quiz\quizy_database.db;Version=3;"; //dostanie sie do pliku .db
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))    //utworzenie polaczenia
             {
                 connection.Open();
 
-                string query = "SELECT id_pytanie, odp_1, odp_2, odp_3, odp_4 from pytania where tresc='" + comboBox_edycja_pytania.SelectedItem+"'";   //sformuowanie zapytania
+                string query = "SELECT id_pytanie, odp_1, odp_2, odp_3, odp_4 from pytania where tresc='" + comboBox_edycja_pytania.SelectedItem + "'";   //sformuowanie zapytania
 
                 SQLiteCommand command = new SQLiteCommand(query, connection);
 
@@ -99,14 +94,14 @@ namespace quiz
 
                 while (reader.Read())
                 {
-                    string id = reader.GetInt32(0).ToString();
+                    id = reader.GetInt32(0);    //przypisanie danych z zapytania do zmiennych
                     string odp1 = reader.GetString(1);
                     string odp2 = reader.GetString(2);
                     string odp3 = reader.GetString(3);
                     string odp4 = reader.GetString(4);
-                    
 
-                    odpowiedz_pierwsza.Text = odp1;
+
+                    odpowiedz_pierwsza.Text = odp1; //wypisanie danych w textboxach
                     odpowiedz_druga.Text = odp2;
                     odpowiedz_trzecia.Text = odp3;
                     odpowiedz_czwarta.Text = odp4;
@@ -114,6 +109,71 @@ namespace quiz
 
                 connection.Close();
             }
+        }
+
+        //--------------------------------dadanie rekordu do bazy-------------------------------
+
+        private void button_dodaj_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=D:\visual\projekty\quiz\quiz\quizy_database.db;Version=3;"; //dostanie sie do pliku .db
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))    //utworzenie polaczenia
+            {
+                connection.Open();
+
+                string pomoc = "do zrobienia";  //utworzenie zmiennych b pozniej je wykorzystac w zapytaniu
+                string tresc = comboBox_edycja_pytania.Text;
+                string odp_1 = odpowiedz_pierwsza.Text;
+                string odp_2 = odpowiedz_druga.Text;
+                string odp_3 = odpowiedz_trzecia.Text;
+                string odp_4 = odpowiedz_czwarta.Text;
+                string odp_poprawna = pomoc;
+
+                string sql = "INSERT INTO pytania (id_quiz, tresc, odp_1, odp_2, odp_3, odp_4, poprawna_odp) VALUES ('" + id_quizu_na_podstawie_nazwy_quizu + "','" + tresc + "','" + odp_1 + "','" + odp_2 + "','" + odp_3 + "','" + odp_4 + "','" + odp_poprawna + "')";  //zapytanie
+
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+
+                // Execute the command
+                command.ExecuteNonQuery();  //wykonanie zapytania
+
+                connection.Close();
+            }
+
+            //----------------------ponowne zaladowanie pytan w comboboxie--------------------------
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))    //utworzenie polaczenia
+            {
+                connection.Open();
+
+                string nazwa = wybor_quizu.nazwa_quizu;
+
+                string query = "SELECT pytania.tresc from pytania INNER JOIN quizy ON pytania.id_quiz = quizy.id_quiz WHERE quizy.nazwa='" + nazwa + "'";   //sformuowanie zapytania
+
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                comboBox_edycja_pytania.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string name = reader.GetString(0);
+                    comboBox_edycja_pytania.Items.Add(name);
+                }
+
+                connection.Close();
+            }
+
+        }
+
+        private void button_modyfikuj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_usun_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
